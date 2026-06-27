4e117bc8ed2cb57a0a255af348d820951dfde97b
jonathanqwong <jonathanqwong@gmail.com>
2026-06-27 10:15:48 -0700

    fix cron expression typo

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ index.listen(port, () => {
 });
 
 // Scheduled function to keep Supabase active (pauses after 7 days inactivity)
-exports.weeklyApiJob = onSchedule('0 */36 * * *', async (event) => {
+exports.weeklyApiJob = onSchedule("0 */36 * * *", async (event) => {
     console.log.info("This runs 36 hours at 12 AM UTC!");
     try {
         const response = await axios.get(ENDPOINT + '/api/skills')
