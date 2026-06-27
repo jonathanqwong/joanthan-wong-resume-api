1836a42be73f291fcc6ffc3b55e4d9ba1a17a352
jonathanqwong <jonathanqwong@gmail.com>
2026-06-27 10:51:52 -0700

    update node version; node 20 deprecation in Oct 26

--- configuration/auth.js
+++ configuration/auth.js
@@ -1,27 +1,5 @@
-// tasks.js (Express route)
-const { auth } = require('express-oauth2-jwt-bearer');
-const { AUDIENCE, ISSUER_BASE_URL} = require("../configuration/configs");
-
-// Authorization middleware. When used, the Access Token must
-// exist and be verified against the Auth0 JSON Web Key Set.
-const checkJwt = auth({
-    audience: AUDIENCE,
-    issuerBaseURL: ISSUER_BASE_URL,
-    tokenSigningAlg: 'RS256'
-});
-
-// Error handling for JWT
 const handleJwtErrors = (err, req, res, next) => {
-    if (err.name === 'UnauthorizedError') {
-        // Custom error response for unauthorized requests
-        res.status(401).json({
-            status: 'error',
-            code: 401,
-            message: 'Unauthorized.'
-        });
-    } else {
-        next(err);
-    }
+    next(err);
 };
 
-module.exports = { checkJwt, handleJwtErrors };
+module.exports = { handleJwtErrors };
